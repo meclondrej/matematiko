35e19d4926fb6a6bb260e49edc0db43cf938b4d5
Ondrej Mecl <119606068+meclondrej@users.noreply.github.com>
2024-06-26 21:49:18 +0200

    Fixed scrollbar always on screen

--- src/App.svelte
+++ src/App.svelte
@@ -225,7 +225,7 @@
         width: 100vw;
         height: 100vh;
         background-color: #000000;
-        overflow: scroll;
+        overflow: auto;
     }
     #panel {
         position: absolute;
